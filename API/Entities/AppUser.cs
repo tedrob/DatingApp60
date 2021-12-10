535535c623b1c8bde042444896d66855b9b62092
tedrobinson <tfqbtd@aol.com>
2021-12-10 12:07:17 -0500

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,8 @@ namespace API.Entities
     public class AppUser
     {
         public int id { get; set; }
-        public string UserName { get; set; }    
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }        
+        public byte[] PasswordSalt { get; set; }        
     }
 }
